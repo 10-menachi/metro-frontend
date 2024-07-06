1838d932cc6e7b415b52fd630875a446d1224644
10-menachi <bobitlmr188221@spu.ac.ke>
2024-07-06 10:46:18 +0300

    Fix bug in code

--- src/components/DashboardContent.jsx
+++ src/components/DashboardContent.jsx
@@ -10,7 +10,7 @@ const DashboardContent = () => {
   const { customers, organisations } = useContext(AppContext);
   const { vehicles } = useContext(VehicleContext);
   const { drivers } = useContext(DriverContext);
-  const isAdmin = user.roles.some((role) => role.name === "admin");
+  const isAdmin = user.role === "admin";
   return (
     <div className="p-4 py-20 sm:ml-64">
       <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
